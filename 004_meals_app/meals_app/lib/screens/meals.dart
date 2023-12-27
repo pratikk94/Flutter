07b3bd512cb294858c78f3eddb2ff305775fec8c
user name <user email>
2023-12-27 20:35:21 +0530

    Meals app

---
+++ 004_meals_app/meals_app/lib/screens/meals.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) => Text(meals[index].title),
+    );
+
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              "No meals found",
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              "Try different cuisine",
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            )
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(appBar: AppBar(title: Text(title)), body: content);
+  }
+}
